fba6a2190f044797f41d7a0867ec73f7238cdf26
mananjabrik <manan.jabrik@gmail.com>
2023-12-05 18:41:08 +0700

    learn cosmwasm

---
+++ src/contract.rs
@@ -0,0 +1,80 @@
+use crate::msg::{GreetResp, QueryMsg};
+use cosmwasm_std::{
+    to_json_binary,
+    Binary, 
+    Deps, 
+    DepsMut, 
+    Empty, 
+    Env, 
+    MessageInfo, 
+    Response, 
+    StdResult, 
+};
+
+pub fn instantiate(
+    _dep:DepsMut, 
+    _env:Env, 
+    _info: MessageInfo, 
+    _msg: Empty
+    )-> StdResult<Response> {
+    Ok(Response::new())
+}
+
+pub fn query(
+    _dep:Deps, 
+    _env:Env, 
+    msg:QueryMsg)-> StdResult<Binary>{
+    use QueryMsg::*;
+    
+    match msg {
+        Greet {} => to_json_binary(&query::greet()?),
+    }
+}
+mod query {
+    use super::*;
+
+    pub fn greet() -> StdResult<GreetResp>{
+        let resp: GreetResp = GreetResp{
+            message: "Hello world".to_owned(),
+        };
+
+        Ok(resp)
+    }
+}
+
+#[allow(dead_code)]
+pub fn execute(_deps:DepsMut, _env:Env, _info: MessageInfo, _msg:Empty)-> StdResult<Response>{
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Addr;
+    use cw_multi_test::{App, ContractWrapper, Executor};    
+
+    use super::*;
+    
+    #[test]
+    fn great_query() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app.instantiate_contract(
+            code_id,
+            Addr::unchecked("owner"),
+            &Empty {}, 
+            &[], 
+            "Contract",
+            None,
+        ).unwrap();
+
+        let resp: GreetResp = app.wrap().query_wasm_smart(addr, &QueryMsg::Greet {}).unwrap();
+
+        assert_eq!(resp, GreetResp {
+                message: "Hello World".to_owned()
+            }
+        );
+    }
+}
