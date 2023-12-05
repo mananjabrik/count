05de61b4dcea820d3ba5e0e5c69f5a74eebe962a
mananjabrik <manan.jabrik@gmail.com>
2023-12-05 18:50:51 +0700

    update

--- src/contract.rs
+++ src/contract.rs
@@ -1,31 +1,20 @@
 use crate::msg::{GreetResp, QueryMsg};
 use cosmwasm_std::{
-    to_json_binary,
-    Binary, 
-    Deps, 
-    DepsMut, 
-    Empty, 
-    Env, 
-    MessageInfo, 
-    Response, 
-    StdResult, 
+    to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
 
 pub fn instantiate(
-    _dep:DepsMut, 
-    _env:Env, 
-    _info: MessageInfo, 
-    _msg: Empty
-    )-> StdResult<Response> {
+    _dep: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty,
+) -> StdResult<Response> {
     Ok(Response::new())
 }
 
-pub fn query(
-    _dep:Deps, 
-    _env:Env, 
-    msg:QueryMsg)-> StdResult<Binary>{
+pub fn query(_dep: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     use QueryMsg::*;
-    
+
     match msg {
         Greet {} => to_json_binary(&query::greet()?),
     }
@@ -33,8 +22,8 @@ pub fn query(
 mod query {
     use super::*;
 
-    pub fn greet() -> StdResult<GreetResp>{
-        let resp: GreetResp = GreetResp{
+    pub fn greet() -> StdResult<GreetResp> {
+        let resp: GreetResp = GreetResp {
             message: "Hello world".to_owned(),
         };
 
@@ -43,17 +32,17 @@ mod query {
 }
 
 #[allow(dead_code)]
-pub fn execute(_deps:DepsMut, _env:Env, _info: MessageInfo, _msg:Empty)-> StdResult<Response>{
+pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> StdResult<Response> {
     unimplemented!()
 }
 
 #[cfg(test)]
 mod tests {
     use cosmwasm_std::Addr;
-    use cw_multi_test::{App, ContractWrapper, Executor};    
+    use cw_multi_test::{App, ContractWrapper, Executor};
 
     use super::*;
-    
+
     #[test]
     fn great_query() {
         let mut app = App::default();
@@ -61,18 +50,25 @@ mod tests {
         let code = ContractWrapper::new(execute, instantiate, query);
         let code_id = app.store_code(Box::new(code));
 
-        let addr = app.instantiate_contract(
-            code_id,
-            Addr::unchecked("owner"),
-            &Empty {}, 
-            &[], 
-            "Contract",
-            None,
-        ).unwrap();
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &Empty {},
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
 
-        let resp: GreetResp = app.wrap().query_wasm_smart(addr, &QueryMsg::Greet {}).unwrap();
+        let resp: GreetResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::Greet {})
+            .unwrap();
 
-        assert_eq!(resp, GreetResp {
+        assert_eq!(
+            resp,
+            GreetResp {
                 message: "Hello World".to_owned()
             }
         );
